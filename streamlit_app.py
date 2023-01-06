ca7f43b9f98736ef7e38631a54a20c04b0e3bbd2
Felix Hoffmann <57142077+felixjhoffmann@users.noreply.github.com>
2023-01-06 11:06:57 +0100

    Update streamlit_app.py
    Added pd.DF index adjustment
    Added Multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,12 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# import dataframe from S3 and set index to fruit
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# display multiselect referencing my_fruit_list index
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# show dataframe
 streamlit.dataframe(my_fruit_list)
