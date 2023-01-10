96466a92bebd32bf0c15a53817d11fd37e8811af
Felix Hoffmann <57142077+felixjhoffmann@users.noreply.github.com>
2023-01-10 18:22:19 +0100

    Update steamlit_simple_app.py

--- steamlit_simple_app.py
+++ steamlit_simple_app.py
@@ -1,1 +1,33 @@
+import streamlit
+import pandas as pd
+import requests
+
+
+streamlit.title('Streamlit\'s Newly Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# import dataframe from S3 and set index to fruit
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# display multiselect referencing my_fruit_list index
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+# create subframe of selected fruits only
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# show dataframe
+streamlit.dataframe(fruits_to_show)
+
+# new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit can we inform you about?','Kiwi')
+streamlit.write('You entered', fruit_choice)
 
